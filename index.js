7708b57271686e01521cd5e45423c677ee46acea
Nohelia Osorio <nosorio1494@gmail.com>
2022-10-20 16:07:52 -0400

    commit tree 668148ef286feb4f5fe15ab85147dc98214430a2

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Write your solution here!
+const cats = [];
+cats [0] = "Milo";
+cats [1] = "Otis";
+cats [2] = "Garfield";
+
+function destructivelyAppendCat() {
+cats.push("Ralph");
+return cats
+}
+
+function destructivelyPrependCat() {
+cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    var newArray = cats.slice();
+    newArray.push(name)
+    return newArray
+  }
+
+function prependCat(name) {
+    // cats.unshift(name);
+    var newArray = cats.slice();
+    newArray.unshift(name);
+    return newArray;
+}
+
+function removeLastCat() {
+//  cats.slice();
+ var newArray = cats.slice();
+    newArray.pop();
+    return newArray;
+}
+
+function removeFirstCat() {
+// cats.shift();
+var newArray = cats.slice();
+newArray.shift();
+return newArray;
+}
